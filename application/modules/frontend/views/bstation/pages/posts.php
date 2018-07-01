59037946be24440b7075d6cba73652c6307dc3d9
Anusha Shetty <anusha@printgreener.com>
2018-07-01 16:12:02 +0530

    Chat issue in business station

--- application/modules/frontend/views/bstation/pages/posts.php
+++ application/modules/frontend/views/bstation/pages/posts.php
@@ -195,7 +195,7 @@ if(count($posts) > 0) {
 						</a>
 					</div><?php //print_r($product);?>
 					<div id="RollOver2" class="img45">
-						<a href="javascript:chat_with123(<?php echo $product['user_id'];?>,<?php echo $contact_details[0]['accept_chat']; ?>);" target="_self"> 
+						<a href="javascript:chat_with(<?php echo $product['user_id'];?>,<?php echo $contact_details[0]['accept_chat']; ?>);" target="_self"> 
 							<img class="hover" alt="Chat" src="<?php echo asset_url(); ?>images/Active/chat_button2.png" /> <span>
 							<img alt="Chat" src="<?php echo asset_url(); ?>images/Link/chat_button2.png" /></span>
 						</a>
